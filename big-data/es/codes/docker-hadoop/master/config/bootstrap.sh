bf2ada3e23d7ebf0a7fc2b1d9721507a30aa91c3
Carlos Cosming <me@ccosming.com>
2020-09-05 16:54:56 -0400

    big-data: Adding Hive to Hadoop Master Image

--- big-data/es/codes/docker-hadoop/master/config/bootstrap.sh
+++ big-data/es/codes/docker-hadoop/master/config/bootstrap.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Iniciando dfs y orquestador yarn.
 service ssh start
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
+
+# Iniciando esquema Hive utilizando el motor derby. 
+$HIVE_HOME/bin/schematool -dbType derby -initSchema
 
 bash
